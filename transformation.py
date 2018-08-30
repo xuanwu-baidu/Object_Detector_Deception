a19d9e36e57c987af694e00b258bae2ce522ceab
Darren <yangyu030000@gmail.com>
2018-08-30 10:16:50 +0800

    Darren add transformation and half photo generation

--- transformation.py
+++ transformation.py
@@ -1,9 +1,11 @@
+#import for transformation
 import numpy as np
-# import tensorflow as tf
-# import cv2
 import math
-
 import eulerangles as eu
+
+#import for image generation
+import cv2
+import xmltodict
 # read a image with corp
 
 # transform it.
@@ -20,9 +22,8 @@ def transform6para(V, transx = 0, transy = 0, transz = 0, rotz = 0, roty = 0, ro
 	return np.dot(Mr, V +  Mt)
 # write the 6 num -> Mx as a function and test it.
 
-if __name__=='__main__':
-
-	# Test Parameter
+def test_transform():
+		# Test Parameter
 	x, y, z, a, b, g = 1, 0, 0, math.pi/2, 0, 0
 	print(x, y, z, a, b, g)
 
@@ -39,3 +40,52 @@ if __name__=='__main__':
 
 	# Test func Transform6para
 	print(transform6para(V, 0, 0, 0, -math.pi/2))
+
+def generate_photo():
+	img = cv2.imread("test/Darren.jpg")
+	height, width, channels = img.shape
+	# rows, cols = image.shape
+	print(height, width, channels)
+
+	f = open("test/Darren.xml")
+	dic = xmltodict.parse(f.read())
+
+	xmin = 0
+	ymin = 0
+	xmax = 0
+	ymax = 0
+	for _object in dic['annotation']['object']:
+		xmin = int(_object['bndbox']['xmin'])
+		ymin = int(_object['bndbox']['ymin'])
+		xmax = int(_object['bndbox']['xmax'])
+		ymax = int(_object['bndbox']['ymax'])
+
+		break
+	print(xmin,ymin,xmax,ymax)
+
+
+	# Shift
+	# M = np.float32([[1, 0, 200], [0, 1, 100]]) 
+
+	# Affine Transformation
+	# transformation here
+	pts1 = np.float32([[50,50],[200,50],[50,200]])
+	pts2 = np.float32([[10,100],[200,50],[100,250]])
+	M = cv2.getAffineTransform(pts1,pts2)
+ 
+	dst = cv2.warpAffine(img, M, (width, height))   
+
+	img_resized = cv2.resize(img, (448, 448))
+	dst_resized = cv2.resize(dst, (448, 448))
+
+	cv2.namedWindow("camera", cv2.WINDOW_NORMAL)
+	cv2.imshow('camera', img_resized)
+	cv2.waitKey()
+
+	cv2.imshow('camera', dst_resized)
+	cv2.waitKey()
+
+if __name__=='__main__':
+	#test_transform()
+	generate_photo()
+
